584a55ec9ff46ce277006761eb49cf6009d5c508
PgLoLo <novgosh@gmail.com>
2019-04-15 12:24:59 +0300

    stylegan refactoring for module like arch

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="stylegan",
+    version="0.0.1",
+    url="https://github.com/neuromation/research-models/feces",
+    packages=find_packages(),
+)
